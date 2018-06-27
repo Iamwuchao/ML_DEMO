76d855b031d712903019de1b105fd251216e1613
wuchao3 <wuchao3@staff.weibo.com>
2018-06-27 20:17:37 +0800

    mmm

--- kaggle/titanic/process.py
+++ kaggle/titanic/process.py
@@ -2,7 +2,21 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
-from titanic.process_data import *
+#from titanic.process_data import *
+from tensorflow.python import debug as tfdbg
+import pandas as pd
+
+train_file = "train.csv"
+test_file = "test.csv"
+
+feature_list = ['Survived','Pclass','Sex','Age','SibSp','Parch','Fare','Cabin','Embarked']
+
+def process_data(file):
+    df = pd.read_csv(file)
+    #print(df.shape)
+    feature_df = df.loc[:,feature_list]
+    #print(feature_df.shape)
+    return feature_df
 
 X_feature = ['Pclass','Sex','Age','SibSp','Parch','Fare','Cabin','Embarked']
 learning_rate = 0.01
@@ -40,22 +54,22 @@ n_samples = train_X.shape[0]
 # X = tf.placeholder("float")
 # Y = tf.placeholder("float")
 
-w0 = tf.Variable(0.1)
-w1 = tf.Variable(tf.truncated_normal([n]))
-w2 = tf.Variable(tf.truncated_normal([n,k]))
+w0 = tf.Variable(0.1,name="w0")
+w1 = tf.Variable(tf.truncated_normal([n]),name='w1')
+w2 = tf.Variable(tf.truncated_normal([n,k],name='w2'))
 
-x_ = tf.placeholder(tf.float32,[None,n])
-y_ = tf.placeholder(tf.float32,[None,1])
+x_ = tf.placeholder(tf.float32,[None,n],name='x_')
+y_ = tf.placeholder(tf.float32,[None,1],name='y_')
 
-batch = tf.placeholder(tf.int32)
+batch = tf.placeholder(tf.int32,name='batch')
 
-w2_new = tf.reshape(tf.tile(w2,[891,1]),[-1,n,k])
+w2_new = tf.reshape(tf.tile(w2,[891,1]),[-1,n,k],name='w2_new')
 
-board_x = tf.reshape(tf.tile(x_,[1,k]),[-1,n,k])
+board_x = tf.reshape(tf.tile(x_,[1,k]),[-1,n,k],name='board_x')
 
-board_x2 = tf.square(board_x)
+board_x2 = tf.square(board_x,name='board_x2')
 
-q = tf.square(tf.reduce_sum(tf.multiply(w2_new,board_x),axis=1))
+q = tf.square(tf.reduce_sum(tf.multiply(w2_new,board_x),axis=1),name='q')
 h = tf.reduce_sum(tf.multiply(tf.square(w2_new),board_x2),axis=1)
 
 y_fm = w0 + tf.reduce_sum(tf.multiply(x_,w1),axis=1) +\
@@ -65,17 +79,19 @@ y_fm = w0 + tf.reduce_sum(tf.multiply(x_,w1),axis=1) +\
 
 #pred = tf.nn.softmax(y_fm)
 #tf.nn.sigmoid_cross_entropy_with_logits(labels=y_,logits=pred)#
-cost = tf.reduce_sum(0.5*tf.square(y_fm-y_))#tf.reduce_mean(-tf.reduce_sum(y_*tf.log(pred)))
+cost = tf.reduce_sum(0.5*tf.square(y_fm-y_),name='cost')#tf.reduce_mean(-tf.reduce_sum(y_*tf.log(pred)))
 
 optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
+    sess = tfdbg.LocalCLIDebugWrapperSession(sess)
+    sess.add_tensor_filter("has_inf_or_nan", tfdbg.has_inf_or_nan)
     sess.run(init)
-    for epoch in range(10):
+    for epoch in range(1000):
         sess.run(optimizer, feed_dict={x_: train_X_onehot, y_: train_Y,batch: 30})
-    print(sess.run(cost,feed_dict={x_: train_X_onehot, y_: train_Y,batch: 30}))
+        print(sess.run(cost,feed_dict={x_: train_X_onehot, y_: train_Y,batch: 30}))
     #print(sess.run(accury, feed_dict={x_: x_test, y_: y_test, batch: 30}))
 
 
